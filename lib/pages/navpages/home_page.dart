3d5aef5b0ce1892826b44e2a2ff85248b300de6a
elijah <elijahkungu100@gmail.com>
2022-09-13 01:17:28 +0300

    travel app ui

---
+++ lib/pages/navpages/home_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:travelapp/pages/widgets/app_large_text.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    TabController _tabController = TabController(length: 3, vsync: this);
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //menu
+          Container(
+            padding: const EdgeInsets.only(top: 70, left: 20),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.menu,
+                  size: 30,
+                  color: Colors.black54,
+                ),
+                Expanded(child: Container()),
+                Container(
+                  margin: const EdgeInsets.only(right: 20),
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey.withOpacity(0.5)),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          //Discover text
+          Container(
+            margin: const EdgeInsets.only(left: 20),
+            child: AppLarge(text: "Discover"),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          //Tapbar
+          Container(
+              child: Align(
+            alignment: Alignment.centerLeft,
+            child: TabBar(
+                labelPadding: const EdgeInsets.only(left: 20, right: 20),
+                controller: _tabController,
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.grey,
+                isScrollable: true,
+                indicatorSize: TabBarIndicatorSize.label,
+                tabs: [
+                  Tab(
+                    text: "Places",
+                  ),
+                  Tab(
+                    text: "Inspiration",
+                  ),
+                  Tab(
+                    text: "Emotions",
+                  )
+                ]),
+          )),
+          Container(
+            height: 300,
+            width: double.maxFinite,
+            child: TabBarView(
+                controller: _tabController,
+                children: [Text("Hi"), Text("There"), Text("Bye")]),
+          )
+        ],
+      ),
+    );
+  }
+}
